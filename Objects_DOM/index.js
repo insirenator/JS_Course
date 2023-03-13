f93d0823cbf53c416a10c5d36278cbe09e8109c1
insirenator <arsshakeeb149@protonmail.com>
2023-03-13 11:52:34 +0530

    Added : Objects_DOM (To-Do App)

---
+++ Objects_DOM/index.js
@@ -0,0 +1,24 @@
+let TASKS = [];
+let tasklist_el = document.querySelector(".tasklist");
+
+function getTaskFromUser() {
+	let userTask = prompt("Enter a Task : ");
+	let date = new Date();
+	let hr = date.getHours();
+	let min = date.getMinutes();
+	let tasktimestamp = hr + ":" + min;
+
+	return { taskname : userTask, timestamp : tasktimestamp } 
+}
+
+function addTask() {
+	task = getTaskFromUser();
+	TASKS.push(task);
+	console.log(task);
+	const update = function (task) {
+		tasklist_el.innerHTML = tasklist_el.innerHTML + `<div class="task">${task.taskname} <span id="timestamp">${task.timestamp}</span></div>`;
+	}(task);
+}
+
+
+
